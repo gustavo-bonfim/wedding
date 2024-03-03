a6c4c9aba468b6cc086704afca93e3e20f812d7d
Gustavo Bonfim <gustavo.hbonfim@outlook.com>
2024-03-03 10:47:12 -0300

    increase map height and remove it from dom in mobile devices

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -83,7 +83,7 @@ function Home() {
             </div>
             <iframe
               title="location map"
-              className="invisible w-1/2 rounded-md border border-wedding lg:visible"
+              className="hidden h-56 w-1/2 rounded-md border border-wedding lg:flex"
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3719.371590288143!2d-50.480062525810446!3d-21.217108579674612!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94964370ac46c477%3A0xb510cb82c579579!2sChacara%20Lighting%20Decor!5e0!3m2!1spt-BR!2sbr!4v1709042664315!5m2!1spt-BR!2sbr"
             />
           </div>
