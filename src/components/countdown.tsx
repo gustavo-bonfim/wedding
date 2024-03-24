0f8885c32e1436ecc77bdda6e745f92ac2fd4918
Gustavo Bonfim <gustavo.hbonfim@outlook.com>
2024-03-24 11:53:09 -0300

    fix countdown grid center in lg

--- src/components/countdown.tsx
+++ src/components/countdown.tsx
@@ -64,7 +64,7 @@ function Countdown() {
   ].filter((item) => item.value > 0);
 
   return (
-    <section className="grid grid-cols-2 gap-4 md:grid-cols-5">
+    <section className={`grid grid-cols-2 gap-4 md:grid-cols-${infos.length}`}>
       {infos.map((info) => (
         <div
           key={info.label}
